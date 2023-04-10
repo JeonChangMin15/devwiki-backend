bab489c5efd5c9e0b2a08a1247ccdb1cebc14b9c
ChangMin <cckdals222@gmail.com>
2023-04-10 14:35:11 +0900

    feat: lecture 삭제 기능추가

--- src/apis/lecture/lecture.resolver.ts
+++ src/apis/lecture/lecture.resolver.ts
@@ -2,17 +2,11 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LectureService } from './lecture.service';
 import { CreateLectureInput } from './dto/createLecture.input';
 import { Lecture } from './entities/lecture.entity';
+import { DeleteLectureResponse } from './dto/deleteLecture.Response';
 
 @Resolver()
 export class LectureResolver {
   constructor(private readonly lectureService: LectureService) {}
-
-  @Mutation(() => Lecture)
-  createLecture(
-    @Args('createLectureInput') createLectureInput: CreateLectureInput,
-  ) {
-    return this.lectureService.create({ createLectureInput });
-  }
 
   @Query(() => [Lecture])
   fetchLectures() {
@@ -22,5 +16,20 @@ export class LectureResolver {
   @Query(() => Lecture)
   fetchLecture(@Args('lectureId') lectureId: string) {
     return this.lectureService.findOne({ lectureId });
+  }
+
+  @Mutation(() => Lecture)
+  createLecture(
+    @Args('createLectureInput') createLectureInput: CreateLectureInput,
+  ) {
+    return this.lectureService.create({ createLectureInput });
+  }
+
+  @Mutation(() => DeleteLectureResponse)
+  deleteLecture(
+    @Args('lectureId') lectureId: string,
+    @Args('password') password: string,
+  ) {
+    return this.lectureService.delete({ lectureId, password });
   }
 }
--- src/apis/lecture/lecture.service.ts
+++ src/apis/lecture/lecture.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Lecture } from './entities/lecture.entity';
 import { Repository } from 'typeorm';
@@ -28,5 +28,20 @@ export class LectureService {
 
   async create({ createLectureInput }) {
     return await this.lectureRepository.save({ ...createLectureInput });
+  }
+
+  async delete({ lectureId, password }) {
+    const lecture = await this.lectureRepository.findOne({
+      where: {
+        id: lectureId,
+      },
+    });
+
+    if (lecture.password !== password)
+      throw new UnauthorizedException('잘못된 비밀번호를 입력했습니다');
+
+    const result = await this.lectureRepository.softDelete({ id: lectureId });
+
+    return { id: lectureId };
   }
 }
